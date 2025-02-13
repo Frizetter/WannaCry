dd9de2743c4e478bf7fb9776d68339471efd0ed3
Frizetter <Ivan17404k@gmail.com>
2025-02-13 22:16:27 +0300

    v0.1
    some new methods and bug fix

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import curses, json
+from datetime import datetime
 def main():
+    screen = curses.initscr()
+    screen.clear()
     while True:
         items = [['1.', 'GetTable'], ['2.', 'CreateTask'], ['3.', 'DeleteTask']]
-
         screen = curses.initscr()
         height, width = screen.getmaxyx()
         curses.start_color()
@@ -33,9 +35,10 @@ def main():
             break
 
 def GetTable():
+    screen = curses.initscr()
+    screen.clear()
     while True:
         screen = curses.initscr()
-        screen.clear()
         height, width = screen.getmaxyx()
         curses.start_color()
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
@@ -47,15 +50,41 @@ def GetTable():
 
         with open('maintable.json', encoding='utf-8') as f:
             data = json.load(f)
+        d = '14.02.2025'
+        weekday = getweekday(d)
+        tasks = data[weekday]
         k = 0
-        for item in data['friday']:
-            line = f'{item} {data['friday'][item]['timestart']} {data['friday'][item]['timefinish']}' 
+        for name, times in sorted(tasks.items(), key=lambda item: item[1]["timestart"]):
+            line = f'{name} {get24fbday(times["timestart"])} - {get24fbday(times["timefinish"])}'
             x = width // 2 - len(line) // 2
-            y = height // 2 - len(data['friday']) // 2 + k
+            y = height // 2 - len(data[weekday]) // 2 + k
             screen.addstr(y, x, line)
             k += 1
-        screen.addstr(height - 2, width // 2 - 13 // 2, 'Press key... ')
+        screen.addstr(height - 2, width // 2 - 13 // 2, '1. go main menu')
         screen.refresh()
+        c = screen.getch()
+        if chr(c) == '1':
+            main()
+            break
+
+def getweekday(d):
+    try:
+        date = datetime.strptime(d, "%d.%m.%Y")
+        day = date.weekday()
+        days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+        return days[day]
+    except ValueError:
+        return 'valueerror'
+    
+def getminsfbday(t):
+    try:
+        hours, mins = t.split(':')
+        return int(hours) * 60 + int(mins)
+    except ValueError:
+        return 'vallueerror'
+def get24fbday(t):
+    t = int(t)
+    return f'{str(t // 60).zfill(2)}:{str(t % 60).zfill(2)}'
 
 def CreateTask():
     print('sdfsdf')
--- testing.py
+++ testing.py
@@ -1,7 +1,23 @@
-import json
+from datetime import datetime
+t = "11:00"
+d = "13.02.2025"
 
-with open('maintable.json', encoding='utf-8') as f:
-    data = json.load(f)
-for item in data['friday']:
-    line = f'{item} {data['friday'][item]['timestart']} {data['friday'][item]['timefinish']}' 
-    print(line)
+def getweekday(d):
+    try:
+        date = datetime.strptime(d, "%d.%m.%Y")
+        day = date.weekday()
+        days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+        return days[day]
+    except ValueError:
+        return 'valueerror'
+    
+def getminsfbday(t):
+    try:
+        hours, mins = t.split(':')
+        return int(hours) * 60 + int(mins)
+    except ValueError:
+        return 'vallueerror'
+    
+if __name__ == '__main__':
+    print(f'{getweekday(d)}')
+    print(f'{getminsfbday(t)}')
